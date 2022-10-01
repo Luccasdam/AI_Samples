2e6e0fd42beecbc0a4c7455556aeade4fe3adfed
Luccasdam <luccas_crims@hotmail.com>
2022-10-01 07:34:10 -0300

    Basic VCS Config

---
+++ FPS_AI_Sample/Source/FPS_AI_Sample/FPS_AI_SampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPS_AI_SampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_AI_SAMPLE_API AFPS_AI_SampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
